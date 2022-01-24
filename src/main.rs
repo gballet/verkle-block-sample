ede00a5be431b515457b5829dab5617aa4e9ca7d
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2022-01-24 11:22:43 +0100

    fix the structure

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-use verkle_trie::proof::VerkleProof;
-use rlp::{Decodable, decode, Rlp, DecoderError};
+use rlp::{decode, Decodable, DecoderError, Rlp};
 use std::fs::File;
 use std::io::Read;
+use verkle_trie::proof::VerkleProof;
+
+struct VerkleKeysValsAndProofs {
+    verkle_proof: VerkleProof,
+    keys: Vec<Vec<u8>>,
+    values: Vec<Option<Vec<u8>>>,
+}
+
+impl Decodable for VerkleKeysValsAndProofs {
+    fn decode(rlp: &Rlp<'_>) -> Result<Self, DecoderError> {
+        todo!();
+    }
+}
+
+struct VerkleHeader {
+    keyvals_and_proof: VerkleKeysValsAndProofs,
+}
+
+impl Decodable for VerkleHeader {
+    fn decode(rlp: &rlp::Rlp<'_>) -> Result<Self, rlp::DecoderError> {
+    }
+}
 
 struct VerkleBlock {
+    header: VerkleHeader,
 }
 
 impl Decodable for VerkleBlock {
@@ -15,7 +37,13 @@ impl Decodable for VerkleBlock {
 fn main() {
     let mut file = File::open("block1.rlp").expect("could not open file");
     let mut serialized = Vec::<u8>::new();
-    file.read_to_end(&mut serialized);
+    file.read_to_end(&mut serialized).unwrap();
 
-    let block : VerkleBlock = decode(&serialized).expect("could not decode verkle block");
+    let block: VerkleBlock = decode(&serialized).expect("could not decode verkle block");
+
+    let kvp = block.header.keyvals_and_proof;
+    //let (checked, _) = kvp.verkle_proof.check(kvp.keys, kvp.values);
+    //if !checked {
+        //panic!("the proof didn't check")
+    //}
 }
